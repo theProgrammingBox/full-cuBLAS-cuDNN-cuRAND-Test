0d95d017c72d306d6f5e4c08c300d7ee2b4d1a99
theProgrammingBox <justinboxgea@gmail.com>
2023-03-20 17:06:01 -0700

    printing version

--- full-cuBLAS-cuDNN-cuRAND-Test/Source.cpp
+++ full-cuBLAS-cuDNN-cuRAND-Test/Source.cpp
@@ -5,8 +5,8 @@
 
 int main()
 {
-	std::cout << "cuDNN version: " << CUDNN_MAJOR << "." << CUDNN_MINOR << "." << CUDNN_PATCHLEVEL << std::endl;
-	std::cout << "cuBLAS version: " << CUBLAS_VER_MAJOR << "." << CUBLAS_VER_MINOR << "." << CUBLAS_VER_PATCH << std::endl;
-	std::cout << "cuRAND version: " << CURAND_VERSION << std::endl;
+	printf("cuDNN version: %d.%d.%d\n", CUDNN_MAJOR, CUDNN_MINOR, CUDNN_PATCHLEVEL);
+	printf("cuBLAS version: %d.%d.%d\n", CUBLAS_VER_MAJOR, CUBLAS_VER_MINOR, CUBLAS_VER_PATCH);
+	printf("cuRAND version: %d.%d.%d\n", CURAND_VERSION / 1000, (CURAND_VERSION % 1000) / 100, CURAND_VERSION % 100);
 	return 0;
 }
